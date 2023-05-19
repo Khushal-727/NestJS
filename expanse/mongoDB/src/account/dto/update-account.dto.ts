63ecc9f8fd60a9ca5c7025372595198a1c4b3508
Khushal Sanjot <124263690+Khushal-727@users.noreply.github.com>
2023-05-19 17:24:08 +0530

    Delete expanse/mongoDB directory

--- expanse/mongoDB/src/account/dto/update-account.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-
-export class UpdateAccountDto {
-  @IsNotEmpty()
-  oldName: string;
-  @IsNotEmpty()
-  newName: string;
-}
--- expanse/mongoDB/src/transaction/dto/update-transaction.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, Min } from 'class-validator';
-import { Type } from 'class-transformer';
-
-export class UpdateTransactionDto {
-  @IsNotEmpty()
-  @Min(1)
-  @Type(() => Number)
-  amount: number;
-}
